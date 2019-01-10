1334ca8919e77b368d63a2aa7e3400191d36693e
Debashish Palit <dpalit07@gmail.com>
2019-01-10 09:39:22 +0530

    Test SSL - 1

--- ssl.py
+++ ssl.py
@@ -1,8 +1,12 @@
+import bottle
+
 class SSLify:
     def __init__(self, app):
         self.app = app
     def __call__(self, environ, start_response):
-        # print('X-Forwarded-Proto is', environ.get('X-Forwarded-Proto'))
+        print('X-Forwarded-Proto is', environ.get('X-Forwarded-Proto'))
+        print(environ)
         if environ.get('X-Forwarded-Proto') == 'http':
-            environ['X-Forwarded-Proto'] = 'https'
+            pass
+            # return bottle.redirect(newurl)
         return self.app(environ, start_response)
